054bf257629de992337cea88625c41230c105d48
jlapierre <jlapierre@safaribooksonline.com>
2018-07-25 06:00:04 -0400

    Initial commit of POM and java classes

---
+++ src/main/java/com/gmail/jnlapierre73/Finddupes.java
@@ -0,0 +1,10 @@
+package com.gmail.jnlapierre73;
+
+public class Finddupes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/gmail/jnlapierre73/FinddupesTest.java
@@ -0,0 +1,14 @@
+package com.gmail.jnlapierre73;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FinddupesTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
